c6db23c634881ce8ab947e20c0d3916ffa61c83d
Sebastien ALONSO <sebastien.alonso@movesol.com>
2019-01-14 10:41:31 +0100

    maven projet

---
+++ src/main/java/Formation_MAVEN/Formation_MAVEN/App.java
@@ -0,0 +1,14 @@
+package Formation_MAVEN.Formation_MAVEN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ src/test/java/Formation_MAVEN/Formation_MAVEN/Test.java
@@ -0,0 +1,10 @@
+package Formation_MAVEN.Formation_MAVEN;
+
+public class  Test
+{
+public static void main (String args [])
+{
+for (int i=0; i<5; i++)
+System.out.println ("indice"+i);
+}
+}
